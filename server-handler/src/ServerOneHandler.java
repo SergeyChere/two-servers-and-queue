850ad0bb997252917e0428b9ec7b2d7222d2864d
Cherepanov Sergey <che.sergii@gmail.com>
2020-03-11 00:56:46 +0200

    second commit

--- server-handler/src/ServerOneHandler.java
+++ server-handler/src/ServerOneHandler.java
@@ -8,23 +8,31 @@ public class ServerOneHandler {
 
     private Storage storage = Storage.getInstance();
     static Socket socket;
-    static int temp;
+
+    static {
+        try {
+            socket = new Socket(InetAddress.getLocalHost().getLocalHost(), 8083);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void handler() {
         while (true) {
             try {
-                temp = storage.consume();
+                int temp = storage.consume();
                 System.out.println("Consumer -> "+temp);
                 if (temp%2==0) {
-                    System.out.println("Creating connection to Server two");
-                    socket = new Socket(InetAddress.getLocalHost().getLocalHost(), 8083);
-                    Thread.sleep(1000);
-                    try (DataOutputStream oos = new DataOutputStream(socket.getOutputStream())) {
-                        while (true) {
-                            System.out.println("writing to sending to server two");
-                            oos.writeInt(temp);
-                            oos.flush();
-                            Thread.sleep(1000);
+                    synchronized (this) {
+                        System.out.println("Creating connection to Server two");
+                        Thread.sleep(1000);
+                        try (DataOutputStream oos = new DataOutputStream(socket.getOutputStream())) {
+                            while (true) {
+                                System.out.println("writing to sending to server two");
+                                oos.writeInt(temp);
+                                oos.flush();
+                                Thread.sleep(1000);
+                            }
                         }
                     }
                 }
